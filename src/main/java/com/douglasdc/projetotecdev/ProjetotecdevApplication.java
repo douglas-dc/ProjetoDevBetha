1711512afcb11bc85c0fd1151948353c9e67266e
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-07-19 10:47:06 -0300

    primeiro commit

---
+++ src/main/java/com/douglasdc/projetotecdev/ProjetotecdevApplication.java
@@ -0,0 +1,13 @@
+package com.douglasdc.projetotecdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotecdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotecdevApplication.class, args);
+	}
+
+}
