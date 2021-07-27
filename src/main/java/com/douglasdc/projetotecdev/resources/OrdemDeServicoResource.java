b0d7045fcf80dbec64103aabc31133eab75cf441
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-07-27 10:37:07 -0300

    Testando o toString com POST

--- src/main/java/com/douglasdc/projetotecdev/resources/OrdemDeServicoResource.java
+++ src/main/java/com/douglasdc/projetotecdev/resources/OrdemDeServicoResource.java
@@ -29,7 +29,7 @@ public class OrdemDeServicoResource {
 
 	@Autowired
 	private OrdemDeServicoService service;
-
+	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<OrdemDeServico> find(@PathVariable Integer id) {
 		OrdemDeServico obj = service.find(id);
@@ -43,6 +43,7 @@ public class OrdemDeServicoResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		System.out.println(obj);
 		return ResponseEntity.created(uri).build();
 	}
 
