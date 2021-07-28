88a73a785e595d83a196c98aec3e443f4cb80999
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-07-28 09:09:09 -0300

    Anotação @Bean para EmailService (MockEmailService)

--- src/main/java/com/douglasdc/projetotecdev/config/TestConfig.java
+++ src/main/java/com/douglasdc/projetotecdev/config/TestConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.douglasdc.projetotecdev.services.DBService;
+import com.douglasdc.projetotecdev.services.EmailService;
+import com.douglasdc.projetotecdev.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -18,5 +20,10 @@ public class TestConfig {
 	public boolean instantiateDatabase() {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 }
