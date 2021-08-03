3a7b61439e5fa74fa8cc308419e1b7c0fcd99dcb
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-08-03 12:00:30 -0300

    Testando envio de email HTML

--- src/main/java/com/douglasdc/projetotecdev/services/MockEmailService.java
+++ src/main/java/com/douglasdc/projetotecdev/services/MockEmailService.java
@@ -1,9 +1,13 @@
 package com.douglasdc.projetotecdev.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MockEmailService extends AbstractEmailService{
 
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
@@ -13,5 +17,12 @@ public class MockEmailService extends AbstractEmailService{
 		LOG.info("Simulando envio de email...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado!");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
 	}
 }
