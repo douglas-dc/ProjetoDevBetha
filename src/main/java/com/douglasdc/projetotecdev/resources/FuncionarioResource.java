0b003c021ff7b6bfbe6b02644e043251db2ab3dc
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-08-09 10:30:38 -0300

    Criando classe Funcionario e respectivo CRUD

---
+++ src/main/java/com/douglasdc/projetotecdev/resources/FuncionarioResource.java
@@ -0,0 +1,38 @@
+package com.douglasdc.projetotecdev.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.douglasdc.projetotecdev.domain.Funcionario;
+import com.douglasdc.projetotecdev.services.FuncionarioService;
+
+@RestController
+@RequestMapping(value="/funcionarios")
+public class FuncionarioResource {
+
+	@Autowired
+	private FuncionarioService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Funcionario> find(@PathVariable Integer id) {
+		Funcionario obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Funcionario obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+}
