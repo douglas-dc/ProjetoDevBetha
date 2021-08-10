67357ca07c3b434daa90e6c148bb354ac2cd9a65
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-08-10 11:41:02 -0300

    Implementando autenticação e geração de Token para segurança

--- src/main/java/com/douglasdc/projetotecdev/repositories/FuncionarioRepository.java
+++ src/main/java/com/douglasdc/projetotecdev/repositories/FuncionarioRepository.java
@@ -2,10 +2,14 @@ package com.douglasdc.projetotecdev.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.douglasdc.projetotecdev.domain.Funcionario;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+	
+	@Transactional(readOnly=true)
+	Funcionario findByEmail(String email);
 
 }
