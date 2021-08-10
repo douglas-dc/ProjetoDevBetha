35c3bc3820f4300e1afb51b4c689b194fa27a5ea
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-08-10 11:47:10 -0300

    Retirando os comentários do código e pequenas alterações

--- src/main/java/com/douglasdc/projetotecdev/repositories/OrdemDeServicoRepository.java
+++ src/main/java/com/douglasdc/projetotecdev/repositories/OrdemDeServicoRepository.java
@@ -1,10 +1,6 @@
 package com.douglasdc.projetotecdev.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.douglasdc.projetotecdev.domain.OrdemDeServico;
@@ -12,7 +8,4 @@ import com.douglasdc.projetotecdev.domain.OrdemDeServico;
 @Repository
 public interface OrdemDeServicoRepository extends JpaRepository<OrdemDeServico, Integer> {
 
-	@Query("SELECT obj FROM OrdemDeServico obj WHERE obj.status = :aprovada")
-	public List<OrdemDeServico> findAprovadas(@Param("aprovada") Integer status);
-	
 }
