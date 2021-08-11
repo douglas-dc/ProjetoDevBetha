c910458590bc662bb6ae1e1d3926d5a6dc752fd2
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-08-11 12:03:21 -0300

    Alterando o PUT de OrdemDeServico e comentando a parte de segurança

---
+++ src/main/java/com/douglasdc/projetotecdev/dto/OrdemDeServicoDTOPut.java
@@ -0,0 +1,66 @@
+package com.douglasdc.projetotecdev.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class OrdemDeServicoDTOPut implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private LocalDate instante;
+	private String equipamentoTipo;
+	private String equipamentoDescricao;
+	private Integer clienteId;
+	
+	public OrdemDeServicoDTOPut() {
+		
+	}
+
+	public OrdemDeServicoDTOPut(LocalDate instante, String equipamentoTipo, String equipamentoDescricao,
+			Integer clienteId) {
+		this.instante = instante;
+		this.equipamentoTipo = equipamentoTipo;
+		this.equipamentoDescricao = equipamentoDescricao;
+		this.clienteId = clienteId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getInstante() {
+		return instante;
+	}
+
+	public void setInstante(LocalDate instante) {
+		this.instante = instante;
+	}
+
+	public String getEquipamentoTipo() {
+		return equipamentoTipo;
+	}
+
+	public void setEquipamentoTipo(String equipamentoTipo) {
+		this.equipamentoTipo = equipamentoTipo;
+	}
+
+	public String getEquipamentoDescricao() {
+		return equipamentoDescricao;
+	}
+
+	public void setEquipamentoDescricao(String equipamentoDescricao) {
+		this.equipamentoDescricao = equipamentoDescricao;
+	}
+
+	public Integer getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteId(Integer clienteId) {
+		this.clienteId = clienteId;
+	}
+}
--- src/main/java/com/douglasdc/projetotecdev/services/UserDetailsServiceImpl.java
+++ src/main/java/com/douglasdc/projetotecdev/services/UserDetailsServiceImpl.java
@@ -1,31 +1,31 @@
-package com.douglasdc.projetotecdev.services;
-
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import com.douglasdc.projetotecdev.domain.Funcionario;
-import com.douglasdc.projetotecdev.repositories.FuncionarioRepository;
-import com.douglasdc.projetotecdev.security.UserSS;
-
-@Service
-public class UserDetailsServiceImpl implements UserDetailsService{
-
-	@Autowired
-	private FuncionarioRepository repo;
-	
-	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		Funcionario func = repo.findByEmail(email);
-		if (func == null) {
-			throw new UsernameNotFoundException(email);
-		}
-		
-		return new UserSS(func.getId(), func.getEmail(), func.getSenha(), Arrays.asList(func.getPerfil()));
-	}
-
-}
+//package com.douglasdc.projetotecdev.services;
+//
+//import java.util.Arrays;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import com.douglasdc.projetotecdev.domain.Funcionario;
+//import com.douglasdc.projetotecdev.repositories.FuncionarioRepository;
+//import com.douglasdc.projetotecdev.security.UserSS;
+//
+//@Service
+//public class UserDetailsServiceImpl implements UserDetailsService{
+//
+//	@Autowired
+//	private FuncionarioRepository repo;
+//	
+//	@Override
+//	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//		Funcionario func = repo.findByEmail(email);
+//		if (func == null) {
+//			throw new UsernameNotFoundException(email);
+//		}
+//		
+//		return new UserSS(func.getId(), func.getEmail(), func.getSenha(), Arrays.asList(func.getPerfil()));
+//	}
+//
+//}
