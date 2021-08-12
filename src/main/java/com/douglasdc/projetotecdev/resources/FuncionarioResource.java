6caf86d49d617c673e5ebc8dd2762859cf6d1da9
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-08-12 12:00:18 -0300

    PUT para Funcionario

--- src/main/java/com/douglasdc/projetotecdev/resources/FuncionarioResource.java
+++ src/main/java/com/douglasdc/projetotecdev/resources/FuncionarioResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
@@ -43,6 +44,13 @@ public class FuncionarioResource {
 	@DeleteMapping(value="/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@RequestBody Funcionario obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 	
